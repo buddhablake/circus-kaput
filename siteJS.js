43871b06d901cdf75b89bea668edad2638c1bcdf
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-04-22 15:13:49 -0700

    completed basic layout & functionality of homepage

--- siteJS.js
+++ siteJS.js
@@ -78,33 +78,10 @@ function animateCards(){
 animateCards();
 
 
+// Round Performer Slider 
 
-//  var slideIndex = 1;
-//  showSlides(slideIndex);
-
-//  // Next/previous controls
-//  function plusSlides(n) {
-//    showSlides(slideIndex += n);
-//  }
-
-//  // Thumbnail image controls
-//  function currentSlide(n) {
-//    showSlides(slideIndex = n);
-//  }
-
-//  function showSlides(n) {
-//    var i;
-//    var slides = document.getElementsByClassName("slides");
-//    var dots = document.getElementsByClassName("dot");
-//    if (n > slides.length) {slideIndex = 1}
-//    if (n < 1) {slideIndex = slides.length}
-//    for (i = 0; i < slides.length; i++) {
-//        slides[i].style.display = "none";
-//    }
-//    for (i = 0; i < dots.length; i++) {
-//        dots[i].className = dots[i].className.replace(" active", "");
-//    }
-//    slides[slideIndex-1].style.display = "block";
-//    dots[slideIndex-1].className += " active";
-//  }
-
+function nextSlide() {
+    var q = function(sel) { return document.querySelector(sel); }   
+    q(".slides").appendChild(q(".slides img:first-child"));
+}
+setInterval(nextSlide, 3500)
